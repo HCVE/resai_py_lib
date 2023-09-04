2a44b3e3472dbd5599b93f6291df595ebf90507b
sitnarf <sitnarf@gmail.com>
2023-09-04 15:09:17 +0200

    stability, reconnecting

--- resai_py_lib/web_socket_connection.py
+++ resai_py_lib/web_socket_connection.py
@@ -1,16 +1,23 @@
 import asyncio
 import json
 import uuid
+from asyncio import CancelledError
 
 import jsonpickle
 from abc import ABC
 from dataclasses import dataclass
 from typing import List, Dict
-from websockets import connect, WebSocketClientProtocol
 
+from traitlets import Callable
+from websockets import connect, WebSocketClientProtocol
+from websockets.exceptions import ConnectionClosedError
 from resai_py_lib.utils import Serializable
-from hcve_lib.utils import convert_to_snake_case_keys, convert_to_snake_case
-from hcve_lib.service_locator import locator
+from hcve_lib.utils import (
+    convert_to_snake_case_keys,
+    convert_to_snake_case,
+    retry_async,
+    find_key,
+)
 
 
 @dataclass
@@ -19,10 +26,31 @@ class Listener:
     prefix: str = ""
 
 
+def print_dict(d, max_length=20, indent=0):
+    """
+    Print a nested dictionary, truncating long values.
+
+    Args:
+    - d (dict): The dictionary to print.
+    - max_length (int): The maximum length for displayed values.
+    - indent (int): Internal parameter for keeping track of indentation.
+    """
+    for key, value in d.items():
+        if isinstance(value, dict):
+            print(" " * indent + str(key) + ":")
+            print_dict(value, max_length, indent + 2)
+        else:
+            display_value = value
+            if isinstance(value, str) and len(value) > max_length:
+                display_value = value[: max_length - 3] + "..."
+            print(" " * indent + f"{key}: {display_value}")
+
+
 class WebSocketConnection(Serializable, ABC):
     web_socket: WebSocketClientProtocol = None
     listeners: List[Listener]
     pending: dict[str, asyncio.Future] = {}
+    on_connection_reconnects: Callable = None
 
     def __init__(self, web_socket: WebSocketClientProtocol = None):
         super().__init__()
@@ -58,7 +86,14 @@ class WebSocketConnection(Serializable, ABC):
         if additional_data is None:
             additional_data = {}
 
-        encoded = jsonpickle.encode(data, unpicklable=unpicklable)
+        if find_key(data, "py/object") or (
+            "args" in data
+            and any(find_key(arg, "py/object") for arg in data["args"])
+            or ("kwargs" in data and find_key(data["kwargs"], "py/object"))
+        ):
+            encoded = json.dumps(data)
+        else:
+            encoded = jsonpickle.encode(data, unpicklable=unpicklable)
 
         if "message_id" not in data:
             message_id = str(uuid.uuid4())
@@ -75,41 +110,51 @@ class WebSocketConnection(Serializable, ABC):
         if additional_data:
             encoded = json.dumps({**json.loads(encoded), **additional_data})
 
-        # print(" → " + encoded)
         await self.web_socket.send(encoded)
-
+        print(" → ", str(encoded)[:200] + "\n")
         if future:
             return future
 
     async def read_and_handle_messages(self):
-        async for message in self.web_socket:
-            message_decoded = convert_to_snake_case_keys(jsonpickle.decode(message))
-            # print(" ← " + str(message_decoded))
-            for listener in self.listeners:
-                prefix = listener.prefix + "_" if listener.prefix else ""
+        try:
+            async for message in self.web_socket:
+                message_decoded = convert_to_snake_case_keys(jsonpickle.decode(message))
+                print(" ← " + str(message_decoded)[:200] + "\n")
+                for listener in self.listeners:
+                    prefix = listener.prefix + "_" if listener.prefix else ""
 
-                message_id = message_decoded.get("message_id", None)
+                    message_id = message_decoded.get("message_id", None)
 
-                if message_id:
-                    future = self.pending.pop(message_id, None)
-                    if future:
-                        future.set_result(message_decoded.get("result", None))
+                    if message_id:
+                        future = self.pending.pop(message_id, None)
+                        if future:
+                            future.set_result(message_decoded.get("result", None))
 
-                method = message_decoded.get("method", None)
-                if method:
-                    result = await getattr(
-                        listener.obj,
-                        convert_to_snake_case(f"{prefix}{method}"),
-                    )(
-                        *message_decoded.get("args", []),
-                        **message_decoded.get("kwargs", {}),
-                    )
-                    if result is not None:
-                        await self.send(
-                            dict(
-                                result=result, message_id=message_decoded["message_id"]
-                            )
+                    method = message_decoded.get("method", None)
+                    if method:
+                        result = await getattr(
+                            listener.obj,
+                            convert_to_snake_case(f"{prefix}{method}"),
+                        )(
+                            *message_decoded.get("args", []),
+                            **message_decoded.get("kwargs", {}),
                         )
+                        if result is not None:
+                            await self.send(
+                                dict(
+                                    result=result,
+                                    message_id=message_decoded["message_id"],
+                                )
+                            )
+        except ConnectionClosedError as e:
+            await self.connection_closed(e)
+            pass
+
+    async def connection_closed(self, e):
+        print(f"Connection closed: {e}")
+
+    def is_connected(self):
+        return self.web_socket and self.web_socket.open
 
 
 class WebSocketClientConnection(WebSocketConnection):
@@ -120,11 +165,18 @@ class WebSocketClientConnection(WebSocketConnection):
         self.port = port
 
     async def call(self, method, *args, **kwargs):
-        if self.web_socket is None:
+        if not self.is_connected():
             await self.connect()
 
         return await super().call(method, *args, **kwargs)
 
+    @retry_async(max_retries=15, retry_delay=1, exception=ConnectionRefusedError)
     async def connect(self):
-        web_socket = await connect(f"ws://localhost:{self.port}")
+        web_socket = await connect(f"ws://localhost:{self.port}", max_size=None)
         self.set_websocket(web_socket)
+
+    async def connection_closed(self, e):
+        print("RECONNECTING")
+        await self.connect()
+        if self.on_connection_reconnects:
+            await self.on_connection_reconnects()
